3a61a2be25af60873337c0b621bf2ac2733ae2b7
Jun Kaneda <kanejun1012@gmail.com>
2024-07-18 22:16:53 +1000

    feat: export types

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,7 +24,14 @@ export default defineConfig({
       }
     }
   },
-  plugins: [vue(), cssInjectedByJsPlugin()],
+  plugins: [
+    vue(),
+    cssInjectedByJsPlugin(),
+    dts({
+      insertTypesEntry: true,
+      copyDtsFiles: true
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
